557deab6ba61d299a7cfd8ff6e18667c137f8c0d
genadi53 <kotka5353@gmail.com>
2023-04-22 18:46:40 +0300

    added card component

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -10,6 +10,7 @@ import { cn } from "../../utils/cn";
 interface Props extends ComponentProps<"textarea"> {
   name: string;
   label: string;
+  hiddenLabel?: boolean;
   className?: string;
 }
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -20,15 +21,17 @@ export const Textarea = forwardRef<HTMLInputElement, Props>((props, ref) => {
   const form = useFormContext();
   const state = form.getFieldState(props.name);
 
+  const { hiddenLabel, ...textareaProps } = props;
+
   return (
     <div>
-      <Label htmlFor={props.name}>{props.label}</Label>
+      {hiddenLabel ? null : <Label htmlFor={props.name}>{props.label}</Label>}
       <textarea
         className={cn(
           "flex h-20 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900",
           props.className
         )}
-        {...props}
+        {...textareaProps}
         id={props.name}
         ref={ref as React.LegacyRef<HTMLTextAreaElement>}
       />
