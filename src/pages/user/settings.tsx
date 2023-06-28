017c5333af128cf7b2408fe906c3aa10050bb4c9
genadi53 <kotka5353@gmail.com>
2023-06-28 16:48:07 +0300

    added settings page

---
+++ src/pages/user/settings.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/src/components/Layout";
+import { type NextPage } from "next";
+
+const UserSettings: NextPage = () => {
+  return (
+    <Layout>
+      <div>User settings</div>
+    </Layout>
+  );
+};
+
+export default UserSettings;
