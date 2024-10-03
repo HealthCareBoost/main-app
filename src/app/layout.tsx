3f62f8d8729b1bdb8d0b85cab1c00b177784f0e2
genadi53 <kotka5353@gmail.com>
2024-10-03 10:28:04 +0300

    add toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,20 @@ import "@/styles/globals.css";
 
 import { type Metadata } from "next";
 import Providers from "@/components/Providers";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "LetMeCook",
+  generator: "Next.js",
+  applicationName: "LetMeCook",
+  referrer: "origin-when-cross-origin",
+  creator: "Genadi Tsolov",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  keywords: ["Next.js", "React", "JavaScript", "Cooking", "Cookbook", "Meals"],
   description: "LetMeCook",
   icons: [{ rel: "icon", url: "/orange.ico" }],
 };
@@ -15,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NextTopLoader color="#fb923c" />
         <Providers>{children}</Providers>
       </body>
     </html>
